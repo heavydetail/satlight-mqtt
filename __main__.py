e1fdb986f4b35f362c713c239cf04e04366200f3
Tamás N <tamas.nyilanszky@gmail.com>
2020-03-11 04:51:04 +0100

    only fetch additional sat data if new sat in Above endpoint

--- __main__.py
+++ __main__.py
@@ -35,9 +35,9 @@ mqttc.username_pw_set('satlight','aboveus')
 MQTT_TOPIC = "satlight/sat"
 
 #API
-API_FETCH_TIMEOUT = 15
+API_FETCH_TIMEOUT = 10
 SATBUFFER_TIMEOUT = 120
-WATCH_DEGREE = 5
+WATCH_DEGREE = 6
 API_KEY = 'R5H4CQ-TJQTXV-BQ53MB-48ZP'
 
 def main():
@@ -72,16 +72,17 @@ def main():
             time.sleep(600) # wait for transactions to reduce.
 
         for i in range(satcount):
-            #fetch additional satellite data for each, TLE
-            r2 = requests.get(url='https://www.n2yo.com/rest/v1/satellite/tle/'+str(data['above'][i]['satid'])+'&apiKey='+API_KEY)
-            satdata = json.loads(r2.text)
-            #print(satdata)
-            print("######### SATDATA ##########")
-            print(satdata['info']['satname'])
-            print("TLE:  \n" + satdata['tle'])
-            tempSat = Satellite(time.time(),data['above'][i]['satid'],satdata['tle'])
-            
+            tempSat = Satellite(time.time(),data['above'][i]['satid'],'')
             if tempSat not in satList: # SAT is not in list, add to list and publish mqtt
+                #fetch additional satellite data for each, TLE
+                r2 = requests.get(url='https://www.n2yo.com/rest/v1/satellite/tle/'+str(data['above'][i]['satid'])+'&apiKey='+API_KEY)
+                satdata = json.loads(r2.text)
+                tempSat.TLE = satdata['tle']
+                #print(satdata)
+                print("######### SATDATA ##########")
+                print(satdata['info']['satname'])
+                print("TLE:  \n" + satdata['tle'])
+            
                 satList.append(tempSat)
                 sendSatMQTT(satdata)
             
